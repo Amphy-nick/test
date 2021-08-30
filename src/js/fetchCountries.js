0cb56ce0c2fed6f943de554871baf62b76d999b5
Burkovskyi Dmytro <79474730+Amphy-nick@users.noreply.github.com>
2021-08-30 15:23:39 +0300

    Initial commit

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+// function FechCountry(value){
+   
+// return fetch(`https://restcountries.eu/rest/v2/name/${value}`)
+// .then(response =>
+//    response.json())
+//    .catch(error => console.log(error))
+// }
+
+// const value = 'Ukraine'
+// console.log(FechCountry())
+
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+   inputSearchCountry: document.querySelector(".search-country-js"),
+   listPushCounries: document.querySelector(".countries-list-js"),
+
+}
